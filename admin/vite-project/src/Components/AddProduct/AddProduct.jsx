aca80e3ec9978e1d4d9207cfc4bde6407423bc10
Saket Kumar <saket356@gmail.com>
2025-08-31 19:07:04 +0530

    feat(admin): fully integrated admin panel with backend, listProduct and addProduct completed

--- admin/vite-project/src/Components/AddProduct/AddProduct.jsx
+++ admin/vite-project/src/Components/AddProduct/AddProduct.jsx
@@ -18,14 +18,43 @@ const AddProduct = () => {
         setimage(e.target.files[0]);
     }
 
-    const changeHandler=(e)=>{
-        setproductDetails({...productDetails,[e.target.name]:[e.target.value]})
-    }
+    const changeHandler = (e) => {
+    setproductDetails({...productDetails, [e.target.name]: e.target.value})
+}
+
 
     const Add_Product = async()=>{
         console.log(productDetails)
+        let responseData;
+        let product=productDetails;
+        let formData= new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                Accept:'application/json'
+            },
+            body:formData,
+        }).then((resp)=>resp.json()).then((data)=>{responseData=data});
+
+         if(responseData.success){
+        product.image=responseData.image_url;
+        console.log(product);   
+        await fetch('http://localhost:4000/addproduct',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(product),
+        }).then((resp)=>resp.json()).then((data)=>{
+            data.success?alert("Product Added"):alert("Failed")
+        })
+    }
     }
 
+   
 
 
   return (
@@ -63,4 +92,4 @@ const AddProduct = () => {
   )
 }
 
-export default AddProduct
+export default AddProduct    
